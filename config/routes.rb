8237f56514465cc3433015b7dce726bfb119bbac
Ubuntu <ubuntu@ip-172-31-46-158.us-west-2.compute.internal>
2015-01-11 21:46:53 +0000

    routes for about

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   get 'terms' => 'terms#index',:as => "terms"
   get 'privacy' => 'privacy#index', :as => "privacy"
+  get 'about' => 'about#index', :as => "about"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
