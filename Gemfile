c0fb5f156662cc063ad40c7b04a8d6fc7f594bb8
Ubuntu <ubuntu@ip-172-31-5-177.us-west-2.compute.internal>
2015-03-28 20:06:10 +0000

    faq model with test

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.8'
 
 gem 'sqlite3'
-gem 'mysql'
+gem 'mysql2'
 gem 'sass-rails', '~> 4.0.3'
 gem 'jquery-rails'
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150412004539) do
 
+  create_table "faqs", force: true do |t|
+    t.text    "question"
+    t.text    "answer"
+    t.text    "image"
+    t.text    "video"
+    t.boolean "visible"
+  end
+
   create_table "items", force: true do |t|
     t.string  "name"
     t.string  "sprite"
@@ -57,7 +65,7 @@ ActiveRecord::Schema.define(version: 20150412004539) do
     t.boolean  "admin"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
